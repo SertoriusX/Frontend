33c9b8bfbda4fe7200c8ab2205f9e48f94fb0d1f
Tsvetan Kirilov Georgiev <152284852+SertoriusX@users.noreply.github.com>
2026-02-19 20:13:23 +0100

    Update PersonInfo.jsx

--- src/components/PersonInfo.jsx
+++ src/components/PersonInfo.jsx
@@ -62,7 +62,9 @@ export default function PersonInfo() {
 
 
  <a
-                href={`${BackEndConnection}/person/download-cv/${person.id}/`}
+                href={`${BackEndConnection}/person/download-cv/${person.id}/?action=view`}
+    target="_blank"
+    rel="noopener noreferrer"
                 className="btn btn-primary btn-custom w-50 rounded-pill p-3 mt-md-5"
               >
                 View CV
