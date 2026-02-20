6283e4d5acac3175704cf400f9e5a1fead404938
Tsvetan Kirilov Georgiev <152284852+SertoriusX@users.noreply.github.com>
2026-02-20 23:22:07 +0100

    Update Portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,19 +1,49 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useState } from 'react';
 import PersonInfo from '../components/PersonInfo';
 import Skill from '../components/Skill';
 import Projects from '../components/Projects';
 import Education from '../components/Education';
 
 export default function Portfolio() {
-   
-    return (
-        <div>
-            <PersonInfo/>
-            <Skill/>
-            <Projects/>
-            <Education/>
-            
+
+  const [activeSection, setActiveSection] = useState("Skill");
+
+  return (
+    <div className="container-fluid ">
+
+      <div className="row">
+
+        <div className="col-12 col-md-7">
+          <PersonInfo />
         </div>
-    );
+
+        <div className="col-12 col-md-5">
+
+          <div className="d-flex justify-content-center mb-3 gap-3">
+            <button
+              className={`btn ${activeSection === "Skill" ? "btn-custom-s" : "btn-custom"}`}
+              onClick={() => setActiveSection("Skill")}
+            >
+              Skill
+            </button>
+
+            <button
+              className={`btn  ${activeSection === "Education" ? "btn-custom-s" : "btn-custom"}`}
+              onClick={() => setActiveSection("Education")}
+            >
+              Education
+            </button>
+          </div>
+
+          {activeSection === "Skill" ? <Skill /> : <Education />}
+
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <Projects />
+      </div>
+
+    </div>
+  );
 }
