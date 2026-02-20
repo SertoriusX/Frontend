fc226104ae43c665a11ad6d38a15c2ee8d712cd0
Tsvetan Kirilov Georgiev <152284852+SertoriusX@users.noreply.github.com>
2026-02-20 23:36:57 +0100

    Update PersonInfo.jsx

--- src/components/PersonInfo.jsx
+++ src/components/PersonInfo.jsx
@@ -84,7 +84,7 @@
                
 
                 <a
-                  href={person.cv_pdf}
+                  href="https://drive.google.com/file/d/1K_oIC7IHZTvh8fgln6VdU7KNRIiwD19D/view"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="btn btn-primary btn-custom w-50 rounded-pill p-3 mt-md-5"
