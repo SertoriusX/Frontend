ec7b592f766d6dfaa35306cc6e3a791a94149bfc
Tsvetan Kirilov Georgiev <152284852+SertoriusX@users.noreply.github.com>
2026-02-22 17:27:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react' 
+import { createRoot } from 'react-dom/client' 
+import './index.css' import App from './App.jsx'
 import { HashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import ProjectDetail from './components/ProjectDetail.jsx';
-import Portfolio from './pages/Portfolio.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
-
+import ProjectDetail from './components/ProjectDetail.jsx' 
+import Portfolio from './pages/Portfolio.jsx' 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // Create routes
 const router = createRoutesFromElements(
   <Route path="/" element={<App />}>
