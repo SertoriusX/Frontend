a7e7d3933d80b5121ad90ca2ca16d187c5610107
Tsvetan Kirilov Georgiev <152284852+SertoriusX@users.noreply.github.com>
2026-02-22 17:30:47 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,21 @@
-import { StrictMode } from 'react' 
-import { createRoot } from 'react-dom/client' 
-import './index.css' import App from './App.jsx'
-import { HashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import ProjectDetail from './components/ProjectDetail.jsx' 
-import Portfolio from './pages/Portfolio.jsx' 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// Create routes
-const router = createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index element={<Portfolio />} />
-    <Route path="projectDetail/:id" element={<ProjectDetail />} />
-  </Route>
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from'react-router-dom'
+import ProjectDetail from './components/ProjectDetail.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 
-// Render with HashRouter to avoid 404 on refresh
+const router =createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<App/>}>
+      <Route index element={<Portfolio />} />
+    <Route path='projectDetail/:id' element={<ProjectDetail/>}/>
+  </Route>
+))
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <RouterProvider router={router} />
-    </HashRouter>
-  </StrictMode>
-);
+    <RouterProvider router={router}/>
+  </StrictMode>,
+) fix it this 
